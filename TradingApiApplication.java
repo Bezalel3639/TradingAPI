9ec7320109fa084ef2968e0e3b14e74e98b2d6ef
Bezalel3639 <bezalel3639@gmail.com>
2019-09-17 12:52:00 +0300

    MongoDB compilation issue fixed

--- TradingApiApplication.java
+++ TradingApiApplication.java
@@ -2,12 +2,20 @@ package com.bezalel.trading_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class TradingApiApplication
-{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(TradingApiApplication.class, args);
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+public class TradingApiApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(TradingApiApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)  {
+    	return builder.sources(TradingApiApplication.class);
     }
 }
