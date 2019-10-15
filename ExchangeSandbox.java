11fca62d36f485b11e8918422a2813d1cd3f05f7
Bezalel3639 <bezalel3639@gmail.com>
2019-10-15 21:15:04 +0300

    readDB_ETHWalletByAddress function added

--- ExchangeSandbox.java
+++ ExchangeSandbox.java
@@ -1,5 +1,7 @@
 package com.bezalel.trading_api;
 
+import static com.mongodb.client.model.Filters.eq;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -11,81 +13,86 @@ import java.util.Map;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.bson.Document;
 import org.json.JSONObject;
 import org.web3j.abi.datatypes.Int;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class ExchangeSandbox {
-    
+	
     public static double getPairQuote(
             String asset1, 
             double asset1_amount, 
             String asset2, 
             String side) {
 
-        Settings settings = new Settings();
-        String uri = settings.uri;
-        double asset1_price = 0;
-        double asset2_price = 0;
-        double assets_rate = 0;	
-        double assets_rate_new = 0;    
-
-        String asset1ID = getAssetId(asset1);
-        String asset2ID = getAssetId(asset2);
-        String arg = asset1ID + "," + asset2ID;
-        
-        if (!side.toUpperCase().equals("BUY") && !side.toUpperCase().equals("SELL")) {
-            System.out.println("Side: 403"); 
-            return -1; // return validation error
-        }
-        if (asset1.toUpperCase() == asset2.toUpperCase()) {
-            System.out.println("403"); // forbidden
-            return -1; // return validation error
-        }
-        if (asset1ID == "404" || asset2ID == "404") {
-            System.out.println("404"); // not found
-            return -1; // return validation error
-        }
-        
-        List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-        parameters.add(new BasicNameValuePair("id", arg)); // <crypto1_id>,<crypto2_id> 
-    
-        try {
-            Waves waves = new  Waves(); // no relation to Waves API
-            String result = waves.makeAPICall(uri, parameters);
-            JSONObject obj = new JSONObject(result);
-            JSONObject objnext = obj.getJSONObject("data");
-            System.out.println(obj.toString()); 
-            JSONObject obj_1 = objnext.getJSONObject(asset1ID);
-            JSONObject obj_2 = objnext.getJSONObject(asset2ID);
-            
-            objnext = obj_1;
-            objnext = objnext.getJSONObject("quote");
-            objnext = objnext.getJSONObject("USD");
-            asset1_price = objnext.getDouble("price");  
-            System.out.println("GetPairQuote> " + asset1 + " price: " + asset1_price + " USD");
-            
-            objnext = obj_2;
-            objnext = objnext.getJSONObject("quote");
-            objnext = objnext.getJSONObject("USD");
-            asset2_price = objnext.getDouble("price");  
-            System.out.println("GetPairQuote> " + asset2 +" price: " + asset2_price + " USD");
-            
-            if (asset1_price == 0) {
-                System.out.println("403"); // forbidden
-                return -1; // return validation error (zero denominator)		    
-            }
-        
-            assets_rate = asset2_price/asset1_price;
-            assets_rate_new = asset1_price/asset2_price;
-        } catch (IOException e) {
-            System.out.println("GetPairQuote> Error: cannont access content - " + e.toString());
-        } catch (URISyntaxException e) {
-            System.out.println("GetPairQuote> Error: Invalid URL " + e.toString());
-        }
-        
-        String pair = asset1.toUpperCase() + "_" + asset2.toUpperCase();
-        System.out.println("GetPairQuote> Pair: " + pair + ", Side: " + side.toUpperCase() + 
-                             ", Fee: " + getFeeValue(pair, side.toUpperCase()));
+    	Settings settings = new Settings();
+    	String uri = settings.uri;
+	    double asset1_price = 0;
+	    double asset2_price = 0;
+	    double assets_rate = 0;	
+	    double assets_rate_new = 0;    
+   
+	    String asset1ID = getAssetId(asset1);
+	    String asset2ID = getAssetId(asset2);
+	    String arg = asset1ID + "," + asset2ID;
+	    
+	    if (!side.toUpperCase().equals("BUY") && !side.toUpperCase().equals("SELL")) {
+	    	System.out.println("Side: 403"); 
+	    	return -1; // return validation error
+	    }
+	    if (asset1.toUpperCase() == asset2.toUpperCase()) {
+	    	System.out.println("403"); // forbidden
+	    	return -1; // return validation error
+	    }
+	    if (asset1ID == "404" || asset2ID == "404") {
+	    	System.out.println("404"); // not found
+	    	return -1; // return validation error
+	    }
+	    
+	    List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+	    parameters.add(new BasicNameValuePair("id", arg)); // <crypto1_id>,<crypto2_id> 
+	
+	    try {
+	    	Waves waves = new  Waves(); // no relation to Waves API
+	        String result = waves.makeAPICall(uri, parameters);
+	   	    JSONObject obj = new JSONObject(result);
+		    JSONObject objnext = obj.getJSONObject("data");
+		    System.out.println(obj.toString()); 
+		    JSONObject obj_1 = objnext.getJSONObject(asset1ID);
+		    JSONObject obj_2 = objnext.getJSONObject(asset2ID);
+		    
+		    objnext = obj_1;
+		    objnext = objnext.getJSONObject("quote");
+		    objnext = objnext.getJSONObject("USD");
+		    asset1_price = objnext.getDouble("price");  
+		    System.out.println("GetPairQuote> " + asset1 + " price: " + asset1_price + " USD");
+		    
+		    objnext = obj_2;
+		    objnext = objnext.getJSONObject("quote");
+		    objnext = objnext.getJSONObject("USD");
+		    asset2_price = objnext.getDouble("price");  
+		    System.out.println("GetPairQuote> " + asset2 +" price: " + asset2_price + " USD");
+		    
+		    if (asset1_price == 0) {
+		    	System.out.println("403"); // forbidden
+		    	return -1; // return validation error (zero denominator)		    
+		    }
+	    
+		    assets_rate = asset2_price/asset1_price;
+		    assets_rate_new = asset1_price/asset2_price;
+	    } catch (IOException e) {
+	        System.out.println("GetPairQuote> Error: cannont access content - " + e.toString());
+	    } catch (URISyntaxException e) {
+	        System.out.println("GetPairQuote> Error: Invalid URL " + e.toString());
+	    }
+	    
+	    String pair = asset1.toUpperCase() + "_" + asset2.toUpperCase();
+	    System.out.println("GetPairQuote> Pair: " + pair + ", Side: " + side.toUpperCase() + 
+	    		             ", Fee: " + getFeeValue(pair, side.toUpperCase()));
         double exchange_rate_buy = (1 + getFeeValue(pair, "BUY"))*asset1_price/asset2_price;
         double exchange_rate_sell = (1 + getFeeValue(pair, "SELL"))*asset1_price/asset2_price;
   
@@ -94,48 +101,49 @@ public class ExchangeSandbox {
         System.out.println("GetPairQuote> 1 " + asset1 + " exchange rate (BUY): " + exchange_rate_buy + " " + asset2); 
         System.out.println("GetPairQuote> 1 " + asset1 + " exchange rate (SELL): " + exchange_rate_sell + " " + asset2); 
         System.out.println("--------------------------------------------------------------------------------");
-        
-        if (assets_rate == 0) {	    	
-            return -1; // return validation error (zero denominator)  
-        }
 
-        // Fee value in percents       
-        double quote = (1 + getFeeValue(pair, side.toUpperCase()))*asset1_amount*assets_rate_new;
-        System.out.println("GetPairQuote> Quote: " + quote);
-        System.out.println("GetPairQuote> You'll need " + quote + " WAVES for " + asset1_amount + " ETH");
         
-        return quote;
-    }
+	    if (assets_rate == 0) {	    	
+	    	return -1; // return validation error (zero denominator)  
+	    }
+
+	    // Fee value in percents
+	    double quote = (1 + getFeeValue(pair, side.toUpperCase()))*asset1_amount*assets_rate_new;
+	    System.out.println("GetPairQuote> Quote: " + quote);
+	    System.out.println("GetPairQuote> You'll need " + quote + " WAVES for " + asset1_amount + " ETH");
+	    
+	    return quote;
+	}
         
     public static String getAssetId(String asset) {
-        
-        String ASSET_NOT_FOUND = "404";
-        
-        Settings settings = new Settings();
-        settings.CollectionCryptoIDs(); // TODO: move to WAR initializer
-        if (settings.cryptoids.get(asset.toUpperCase()) == null) {
-            return ASSET_NOT_FOUND;  
-        }
-        
-        System.out.println(settings.cryptoids.get(asset.toUpperCase()));
-        
-        return settings.cryptoids.get(asset.toUpperCase());
+    	
+    	String ASSET_NOT_FOUND = "404";
+    	
+		Settings settings = new Settings();
+		settings.CollectionCryptoIDs(); // TODO: move to WAR initializer
+		if (settings.cryptoids.get(asset.toUpperCase()) == null) {
+			return ASSET_NOT_FOUND;  
+		}
+		
+		System.out.println(settings.cryptoids.get(asset.toUpperCase()));
+		
+		return settings.cryptoids.get(asset.toUpperCase());
     }
      
     public static double getFeeValue(String pair, String side) {
-        
-        // Format sample: BTC_WAVES_SELL
-        String value = pair.toUpperCase() + "_" + side.toUpperCase();
-          
-        Settings settings = new Settings();
-        settings.CollectionCurrencyPairs(); // TODO: move to WAR initializer
-        if (settings.pairs.get(value) == null) {
-            System.out.println("The pair is not registered");  
-        }
-         
-        return settings.pairs.get(value);
-    }   
-     
+    	
+     	// Format sample: BTC_WAVES_SELL
+      	String value = pair.toUpperCase() + "_" + side.toUpperCase();
+      	
+ 		Settings settings = new Settings();
+ 		settings.CollectionCurrencyPairs(); // TODO: move to WAR initializer
+		if (settings.pairs.get(value) == null) {
+			System.out.println("The pair is not registered");  
+		}
+     	
+ 		return settings.pairs.get(value);
+    }    
+   
     public String ExchangePair_ETH2WAVES(
               String type,               
               double amount,                           
@@ -145,8 +153,8 @@ public class ExchangeSandbox {
  
         String result = null;
         String asset_base = "ETH"; 
-        String asset_quoted  = "WAVES";         
-       
+        String asset_quoted  = "WAVES"; 
+        
         if (!type.toUpperCase().equals("BUY") && !type.toUpperCase().equals("SELL")) {
             System.out.println("Side: 403"); 
             // Return validation error ("Buy" or "Sell" are the only allowed values)
@@ -213,51 +221,51 @@ public class ExchangeSandbox {
                 
                 break;
             case "BUY":                
-                // Get quote
-                amount_eth = 0;
-                if (fixed_base_amount) {
-                    quote = getPairQuote(asset_base, amount, asset_quoted, type);
-                    System.out.println("Quote here: " + quote);
-                }
-                else if (!fixed_base_amount) { 
-                    quote = getPairQuote(asset_base, 1, asset_quoted, type); //1 ETH = XXX WAVES
-                    System.out.println("1 ETH is " + quote + " WAVES");
-                    amount_eth = amount/quote;
-                }
+                  // Get quote
+                  amount_eth = 0;
+                  if (fixed_base_amount) {
+                      quote = getPairQuote(asset_base, amount, asset_quoted, type);
+                      System.out.println("Quote here: " + quote);
+                  }
+                  else if (!fixed_base_amount) { 
+                      quote = getPairQuote(asset_base, 1, asset_quoted, type); //1 ETH = XXX WAVES
+                      System.out.println("1 ETH is " + quote + " WAVES");
+                      amount_eth = amount/quote;
+                  }
 
-                double amount_log = 0;
-                if (fixed_base_amount) {                      
-                    first_tx_res = SendETH2ETH(amount /* ETH */, asset_base_address, asset_base_reservesaddress);
-                    amount_log = amount;
-                    System.out.println("ETH amount: " + amount + " ETH");
-                } else {
-                    BigDecimal bd = BigDecimal.valueOf(amount_eth).setScale(10, BigDecimal.ROUND_HALF_UP);
-                    System.out.println("ETH amount (original): " + amount_eth + "ETH");
-                    amount_log = bd.doubleValue();                      
-                    first_tx_res = SendETH2ETH(bd.doubleValue() /* ETH */, asset_base_address, asset_base_reservesaddress); 
-                }                  
-                System.out.println("--------------------------------------------------------------------------------");
-                System.out.println("ETH amount: " + amount_log + " ETH");
-                System.out.println("ETH address (from): " + asset_base_address);
-                System.out.println("ETH address (to): " + asset_base_reservesaddress);
-                System.out.println("--------------------------------------------------------------------------------");
-                
-                // TODO: check error (if the result of 1st tx): abort
-                 
-                if (fixed_base_amount) {
-                    second_tx_res = SendWaves2Waves(quote /* WAVES */, asset_quoted_reservesaddress, asset_quoted_address);
-                    amount_log = quote;
-                } else {
-                    second_tx_res = SendWaves2Waves(amount /* WAVES */, asset_quoted_reservesaddress, asset_quoted_address);
-                    amount_log = amount;
-                }
-                System.out.println("--------------------------------------------------------------------------------");
-                System.out.println("WAVES amount (quote): " + amount_log);
-                System.out.println("WAVES address (from): " + asset_quoted_reservesaddress);
-                System.out.println("WAVES address (to): " + asset_quoted_address);
-                System.out.println("--------------------------------------------------------------------------------");
-                
-                // TODO: check error (if the result of 2ndt tx): abort + return ETH
+                  double amount_log = 0;
+                  if (fixed_base_amount) {                      
+                      first_tx_res = SendETH2ETH(amount /* ETH */, asset_base_address, asset_base_reservesaddress);
+                      amount_log = amount;
+                      System.out.println("ETH amount: " + amount + " ETH");
+                  } else {
+                      BigDecimal bd = BigDecimal.valueOf(amount_eth).setScale(10, BigDecimal.ROUND_HALF_UP);
+                      System.out.println("ETH amount (original): " + amount_eth + "ETH");
+                      amount_log = bd.doubleValue();                      
+                      first_tx_res = SendETH2ETH(bd.doubleValue() /* ETH */, asset_base_address, asset_base_reservesaddress); 
+                  }                  
+                  System.out.println("--------------------------------------------------------------------------------");
+                  System.out.println("ETH amount: " + amount_log + " ETH");
+                  System.out.println("ETH address (from): " + asset_base_address);
+                  System.out.println("ETH address (to): " + asset_base_reservesaddress);
+                  System.out.println("--------------------------------------------------------------------------------");
+                  
+                  // TODO: check error (if the result of 1st tx): abort
+                  
+                  if (fixed_base_amount) {
+                      second_tx_res = SendWaves2Waves(quote /* WAVES */, asset_quoted_reservesaddress, asset_quoted_address);
+                      amount_log = quote;
+                  } else {
+                      second_tx_res = SendWaves2Waves(amount /* WAVES */, asset_quoted_reservesaddress, asset_quoted_address);
+                       amount_log = amount;
+                  }
+                  System.out.println("--------------------------------------------------------------------------------");
+                  System.out.println("WAVES amount (quote): " + amount_log);
+                  System.out.println("WAVES address (from): " + asset_quoted_reservesaddress);
+                  System.out.println("WAVES address (to): " + asset_quoted_address);
+                  System.out.println("--------------------------------------------------------------------------------");
+                  
+                  // TODO: check error (if the result of 2ndt tx): abort + return ETH
                   
                 break;
             default:
@@ -313,13 +321,13 @@ public class ExchangeSandbox {
         
         Settings settings = new Settings();
         settings.CollectionKeys();        
-     
-        // Get Waves seed       
+      
+        // Get Waves seed      
         JSONObject obj = new JSONObject(settings.wavesobj.toString());
         System.out.println(obj.get(address_from)); 
         String sourceseed = obj.get(address_from).toString(); 
       
-        // TODO: check if Waves balance is enough and exceeds minimum TX value
+        // TODO: check if Waves balance is enough and exceeds minimum TX value (TODO)
         WavesTestnet waves = new  WavesTestnet();
         long userbalance = waves.getBalance(address_from);
         System.out.println("Waves user balance: " + String.valueOf(userbalance));
@@ -374,21 +382,21 @@ public class ExchangeSandbox {
         return result;
     }
     
-    public static String SendETH2ETH(
+    public String SendETH2ETH(
             double amount, 
             String addressfrom, // from reserves if ETH_WAVES_BUY
             String addressto) throws Exception {
         
         Settings settings = new Settings();
-        settings.CollectionKeys();      
-     
-        // Get Waves seed f  
+        settings.CollectionKeys();        
+       
+        // Get Waves seed   
         JSONObject obj = settings.locateETHWalletByAddress(addressfrom);
         System.out.println(obj.get("wallet")); 
         String walletfrom = obj.getString("wallet"); 
         String password = obj.getString("password");
         System.out.println("Wallet from: " + walletfrom); 
-
+        
         // TODO: check if ETH balance is enough and exceeds minimum TX value 
         EthereumTestnet_web3j eth = new EthereumTestnet_web3j();        
         BigInteger addressfrom_balance = eth.getBalance(addressfrom);
@@ -406,7 +414,7 @@ public class ExchangeSandbox {
         
         String res = eth.SendETH(walletfile, password, addressto, amount /* ETH */);        
         System.out.println("SendETH2ETH, TX result: " + res); 
-        
+         
         return res;
     }
     
@@ -427,4 +435,25 @@ public class ExchangeSandbox {
         
         return result; 
     }
+    
+    public JSONObject readDB_ETHWalletByAddress(String address) throws Exception {
+        
+        Settings settings = new Settings();
+        MongoClient mongoClient = new MongoClient(settings.mongodb_host, settings.mongodb_port);       
+             
+        MongoDatabase database = mongoClient.getDatabase("wallets");
+        MongoCollection<Document> collection = database.getCollection("ETH (testnets)");
+
+        Document document = collection.find(eq("address", address)).first();
+        if (document == null) {             
+            System.out.println("Nothing was found in DB");
+            mongoClient.close(); 
+            return null;
+        }; 
+         
+        JSONObject obj = new JSONObject(document.toJson());    
+        mongoClient.close(); 
+
+        return obj;
+    }   
 }
