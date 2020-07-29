e203d7398e2b707a02375b64238b73294ed6aa6e
Bezalel3639 <bezalel3639@gmail.com>
2020-07-29 19:04:12 +0300

    Multi-user multicurrency wallet enabled

--- Utils.java
+++ Utils.java
@@ -16,13 +16,16 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESedeKeySpec;
+
 import org.apache.commons.codec.binary.Base64;
 import org.bson.Document;
-import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONObject; 
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
@@ -51,6 +54,104 @@ public class Utils {
         results_obj.put("address_to", address_to);
         results_obj.put("tx_result", tx_result);  
         results_obj.put("execution_time", execution_time);  
+        
+        // Insert TX results into MongoDB        
+        try {
+            MongoCredential credential = MongoCredential.createCredential(
+                    settings.user, 
+                    db_name, 
+                    settings.password.toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress(
+                    Settings.mongodb_host, 
+                    settings.mongodb_port),
+                    Arrays.asList(credential));
+            
+            Document doc = Document.parse(results_obj.toString());
+            mongoClient.getDatabase(db_name).getCollection(collection).insertOne(doc);
+            mongoClient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+        return results_obj.toString();
+    }
+    
+    public String logFiatDepositResults (
+            String currency,
+            double amount,
+            double transaction_fee,
+            String provider, 
+            String emailfrom,
+            String emailto,
+            String payer_id,
+            String payment_id,
+            String token, 
+            String tx_result,
+            String notes) throws Exception {
+         
+        Settings settings = new Settings();
+        String db_name = "logs";
+        String collection = "sandbox_fiat_deposits";
+        
+        JSONObject results_obj = new JSONObject();
+        String date = new Date().toString();
+        results_obj.put("date", date);        
+        results_obj.put("currency", currency); 
+        results_obj.put("amount", amount);
+        results_obj.put("fee", transaction_fee);
+        results_obj.put("provider", provider);
+        results_obj.put("emailfrom", emailfrom);
+        results_obj.put("emailto", emailto);
+        results_obj.put("payer_id", payer_id);  
+        results_obj.put("payment_id", payment_id); 
+        results_obj.put("token", token);
+        results_obj.put("tx_result", tx_result); 
+        results_obj.put("notes", notes);    
+        
+        // Insert TX results into MongoDB        
+        try {
+            MongoCredential credential = MongoCredential.createCredential(
+                    settings.user, 
+                    db_name, 
+                    settings.password.toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress(
+                    Settings.mongodb_host, 
+                    settings.mongodb_port),
+                    Arrays.asList(credential));
+            
+            Document doc = Document.parse(results_obj.toString());
+            mongoClient.getDatabase(db_name).getCollection(collection).insertOne(doc);
+            mongoClient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+        return results_obj.toString();
+    }
+    
+    public String logFiatWithdrawalResults (
+            String currency,
+            double amount,
+            String provider, 
+            String emailfrom,
+            String emailto,
+            String tx_result,
+            String notes) throws Exception { 
+        
+        Settings settings = new Settings();
+        String db_name = "logs";
+        String collection = "sandbox_fiat_withdrawals";
+        
+        JSONObject results_obj = new JSONObject();
+        String date = new Date().toString();
+        results_obj.put("date", date);        
+        results_obj.put("currency", currency); 
+        results_obj.put("amount", amount);
+        results_obj.put("provider", provider);
+        results_obj.put("emailfrom", emailfrom);
+        results_obj.put("emailto", emailto);
+        results_obj.put("tx_result", tx_result); 
+        results_obj.put("notes", notes);  
         
         // Insert TX results into MongoDB        
         try {
@@ -210,20 +311,48 @@ public class Utils {
         results_obj.put("private_key", pk_encrypted); 
         results_obj.put("network", network); 
         results_obj.put("user", user);
-        results_obj.put("date_created",  date);
+        results_obj.put("date_created", date);
         results_obj.put("wallet", walletdata_encrypted);
         results_obj.put("notes", notes);
         
         return results_obj.toString();
     }
     
-    public String makeJSONFromWallet (
+    public String makeJSON_BTCWallet (
+            String address, 
+            String pk_encrypted, 
+            String network, 
+            String user, 
+            String walletdata_encrypted,
+            String provider,
+            String notes) throws Exception { 
+        
+        JSONObject results_obj = new JSONObject();
+        
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        String date = dateFormat.format(new Date());
+        
+        results_obj.put("symbol", "BTC");
+        results_obj.put("address", address); 
+        results_obj.put("private_key", pk_encrypted); 
+        results_obj.put("network", network); 
+        results_obj.put("user", user);
+        results_obj.put("date_created", date);
+        results_obj.put("wallet", walletdata_encrypted);
+        results_obj.put("provider", provider);
+        results_obj.put("notes", notes);
+        
+        return results_obj.toString();
+    }
+    
+    public String makeJSON_ETHWallet (
             String address, 
             String password_encrypted,
             String network,
             String user, 
             String walletname, 
-            String wallet_data) throws Exception { 
+            String wallet_data,
+            String provider) throws Exception { 
         
         JSONObject results_obj = new JSONObject();
         
@@ -234,7 +363,7 @@ public class Utils {
         results_obj.put("password", password_encrypted); 
         results_obj.put("network", network); 
         results_obj.put("user", user); 
-        results_obj.put("date_created",  date); 
+        results_obj.put("date_created", date); 
 
         JSONObject wallet_obj = new JSONObject();
         wallet_obj.put("name", walletname);
@@ -242,7 +371,7 @@ public class Utils {
         JSONObject dataobj = new JSONObject(wallet_data);
         wallet_obj.put("data", dataobj);            
         results_obj.put("wallet", wallet_obj);       
-         
+        results_obj.put("provider", provider); 
         results_obj.put("notes", "Valid for other testnets and mainnet also");
         
         return results_obj.toString();
@@ -266,13 +395,13 @@ public class Utils {
         results_obj.put("seed", seed_encrypted); 
         results_obj.put("network", network); 
         results_obj.put("user", user);
-        results_obj.put("date_created",  date);
+        results_obj.put("date_created", date);
         results_obj.put("wallet", walletdata_encrypted);
         results_obj.put("notes", notes);
         
         return results_obj.toString();
-    }
-    
+    }    
+
     public String readLine(String ffn) throws Exception {
         File file = new File(ffn); 
         BufferedReader br = new BufferedReader(new FileReader(file)); 
@@ -374,5 +503,31 @@ public class Utils {
         result.put("status", "200");
         result.put("data", "Database connection is verified!");
         return result;    
-    }  
+    }    
+    
+    public String utility_getRandomString() {        
+        int leftLimit = 49;  // symbol '1'
+        int rightLimit = 57; // symbol '9'
+        int targetStringLength = 9;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int) 
+              (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();     
+        System.out.println("GetRandomString> " + generatedString); 
+        
+        return generatedString;
+    }      
+
+    public double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
 }
