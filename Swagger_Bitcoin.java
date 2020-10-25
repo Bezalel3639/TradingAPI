fa6d215340d5422345da669309c8ef0c4d55dfee
Bezalel3639 <bezalel3639@gmail.com>
2020-10-25 14:18:53 +0300

    SendFromAddress is fixed for calls from site

--- Swagger_Bitcoin.java
+++ Swagger_Bitcoin.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.math.BigInteger;
 
@@ -23,7 +24,7 @@ public class Swagger_Bitcoin {
     
     @RequestMapping(path="/Bitcoin/IsAddressValid/{address}", method = RequestMethod.GET)
     @ApiOperation(value="Check if address is valid", tags="Bitcoin API")
-    public boolean IsAddressValid(String address) throws Exception { 
+    public boolean IsAddressValid(@PathVariable String address) throws Exception {  
         Bitcoin_Blockcypher bitcoin = new Bitcoin_Blockcypher();
         return bitcoin.validateAddress(address); 
     }
@@ -44,7 +45,7 @@ public class Swagger_Bitcoin {
             return new ResponseEntity<>(result.get("data").toString(), HttpStatus.OK);  
         }
     }   
- 
+
     @RequestMapping(path="/Bitcoin/Balance/{address}", method = RequestMethod.GET)
     @ApiOperation(value="Get balance", tags="Bitcoin API")
     public long getBalanceBlockcypher(@PathVariable String address) throws Exception {
@@ -65,9 +66,9 @@ public class Swagger_Bitcoin {
     	return result; 
     }
     
-    @RequestMapping(path="/Bitcoin/BTCRate/", method = RequestMethod.GET)
+    @RequestMapping(path="/Bitcoin/BlockchainInfo/BTCRate/", method = RequestMethod.GET)
 	@ApiOperation(value="Get BTC rate in USD", tags="Bitcoin API")
-    public String getBTCRate() throws Exception {
+    public String getBTCBlockchainInfoRate() throws Exception {
      	String result = null;
     	
   	  	Bitcoin_BlockchainInfo bitcoin = new Bitcoin_BlockchainInfo();
@@ -76,13 +77,20 @@ public class Swagger_Bitcoin {
     	return result; 
     }
     
-    @RequestMapping(path="/Bitcoin/SendFromAddress/{addressfrom}/{addressto}/{amount}/{password}", method = RequestMethod.POST)
+    @RequestMapping(path="/Bitcoin/BTCRate/", method = RequestMethod.GET)
+    @ApiOperation(value="Get BTC rate in USD", tags="Bitcoin API")
+    public double getBTCRate() throws Exception {
+        Bitcoin_Blockcypher bitcoin = new Bitcoin_Blockcypher();
+        return bitcoin.getBTCRate();
+    }
+    
+    @RequestMapping(path="/Bitcoin/SendFromAddress/", method = RequestMethod.POST)
     @ApiOperation(value="Send BTC", tags="Bitcoin API")
-    public ResponseEntity<String> sendBTC(
-            @PathVariable @ApiParam(defaultValue = "15CCuZX353pBagPdbLmCimFwQLNtAP82JX") String addressfrom, 
-            @PathVariable @ApiParam(defaultValue = "1Dy7ZKCxKt51rDYkr6tjvNcX1ivuVQDFSC") String addressto, 
-            @PathVariable @ApiParam(defaultValue = "0.00011") double amount,
-            @PathVariable String password) throws Exception {  
+    public ResponseEntity<String> sendBTC(            
+            @RequestParam(value="addressfrom", required=true) @ApiParam(defaultValue = "15CCuZX353pBagPdbLmCimFwQLNtAP82JX") String addressfrom,
+            @RequestParam(value="addressto", required=true) @ApiParam(defaultValue = "1Dy7ZKCxKt51rDYkr6tjvNcX1ivuVQDFSC") String addressto, 
+            @RequestParam(value="amount", required=true) @ApiParam(defaultValue = "0.0001") double amount,
+            @RequestParam(value="password", required=true)  String password) throws Exception { 
     
         // Validate password
         Settings settings = new Settings();
