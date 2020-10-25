dd1bf249f04eed635b1c732a6161d28428cd8efd
Bezalel3639 <bezalel3639@gmail.com>
2020-10-25 20:01:49 +0300

    Added logFiatDepositResultsFromYandex, logFiatWithdrawalResultsFromYandex

--- Utils.java
+++ Utils.java
@@ -16,16 +16,13 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
-
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESedeKeySpec;
-
 import org.apache.commons.codec.binary.Base64;
 import org.bson.Document;
-import org.codehaus.jettison.json.JSONObject; 
-
+import org.codehaus.jettison.json.JSONObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
@@ -106,7 +103,57 @@ public class Utils {
         results_obj.put("payment_id", payment_id); 
         results_obj.put("token", token);
         results_obj.put("tx_result", tx_result); 
-        results_obj.put("notes", notes);   
+        results_obj.put("notes", notes); 
+        
+        // Insert TX results into MongoDB        
+        try {
+            MongoCredential credential = MongoCredential.createCredential(
+                    settings.user, 
+                    db_name, 
+                    settings.password.toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress(
+                    Settings.mongodb_host, 
+                    settings.mongodb_port),
+                    Arrays.asList(credential));
+            
+            Document doc = Document.parse(results_obj.toString());
+            mongoClient.getDatabase(db_name).getCollection(collection).insertOne(doc);
+            mongoClient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+        return results_obj.toString();
+    }
+    
+    public String logFiatDepositResultsFromYandex (
+            String currency,
+            double amount,
+            double transaction_fee,
+            String emailfrom,
+            String emailto,
+            String label,
+            String transaction_id, 
+            String tx_result,
+            String notes) throws Exception { 
+        
+        Settings settings = new Settings();
+        String db_name = "logs";
+        String collection = "fiat_deposits";
+        
+        JSONObject results_obj = new JSONObject();
+        String date = new Date().toString();
+        results_obj.put("date", date);        
+        results_obj.put("currency", currency); 
+        results_obj.put("amount", amount);
+        results_obj.put("fee", transaction_fee);
+        results_obj.put("provider", "Yandex.Money");
+        results_obj.put("emailfrom", emailfrom);
+        results_obj.put("emailto", "stan1368@yandex.ru");
+        results_obj.put("label", label); 
+        results_obj.put("transaction_id", transaction_id);
+        results_obj.put("tx_result", tx_result); 
+        results_obj.put("notes", notes);  
         
         // Insert TX results into MongoDB        
         try {
@@ -151,8 +198,55 @@ public class Utils {
         results_obj.put("emailfrom", emailfrom);
         results_obj.put("emailto", emailto);
         results_obj.put("tx_result", tx_result); 
+        results_obj.put("notes", notes);
+        
+        // Insert TX results into MongoDB        
+        try {
+            MongoCredential credential = MongoCredential.createCredential(
+                    settings.user, 
+                    db_name, 
+                    settings.password.toCharArray());
+            MongoClient mongoClient = new MongoClient(new ServerAddress(
+                    Settings.mongodb_host, 
+                    settings.mongodb_port),
+                    Arrays.asList(credential));
+            
+            Document doc = Document.parse(results_obj.toString());
+            mongoClient.getDatabase(db_name).getCollection(collection).insertOne(doc);
+            mongoClient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+        return results_obj.toString();
+    }
+    
+    public String logFiatWithdrawalResultsFromYandex (
+            String currency,
+            double amount,
+            String provider, 
+            String emailfrom,
+            String emailto,
+            String tx_id,
+            String tx_result,
+            String notes) throws Exception { 
+        
+        Settings settings = new Settings();
+        String db_name = "logs";
+        String collection = "fiat_withdrawals";
+        
+        JSONObject results_obj = new JSONObject();
+        String date = new Date().toString();
+        results_obj.put("date", date);        
+        results_obj.put("currency", currency); 
+        results_obj.put("amount", amount);
+        results_obj.put("provider", provider);
+        results_obj.put("emailfrom", emailfrom);
+        results_obj.put("emailto", emailto);
+        results_obj.put("transaction_id", tx_id);
+        results_obj.put("tx_result", tx_result); 
         results_obj.put("notes", notes); 
-         
+        
         // Insert TX results into MongoDB        
         try {
             MongoCredential credential = MongoCredential.createCredential(
@@ -533,8 +627,8 @@ public class Utils {
     }    
     
     public String utility_getRandomString() {        
-        int leftLimit = 49;  // symbol '1'
-        int rightLimit = 57; // symbol '9'
+        int leftLimit = 49;  
+        int rightLimit = 57; 
         int targetStringLength = 9;
         Random random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
